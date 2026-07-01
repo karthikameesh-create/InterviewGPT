aee462c76f750a54cb02c4831d0b7feea452279d
Ameesh Karthik <karthikameesg@gmail.com>
2026-07-01 23:16:04 +0530

    Fix deployment dependencies

--- utils/firebase_config.py
+++ utils/firebase_config.py
@@ -1,10 +1,33 @@
+import os
+import json
 import firebase_admin
+import streamlit as st
 from firebase_admin import credentials
 
-# Initialize Firebase Admin SDK only once
+# =====================================
+# FIREBASE INITIALIZATION
+# =====================================
+
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+
+    try:
+        # Streamlit Cloud
+        firebase_dict = dict(st.secrets["firebase"])
+
+        cred = credentials.Certificate(firebase_dict)
+
+    except Exception:
+        # Local Development
+        cred = credentials.Certificate("firebase_key.json")
+
     firebase_admin.initialize_app(cred)
 
-# Firebase Web API Key
-FIREBASE_API_KEY = "AIzaSyCea-1e3d58-yWPRGKLV_IJxSCzOvvnfRA"
+# =====================================
+# FIREBASE WEB API KEY
+# =====================================
+
+try:
+    FIREBASE_API_KEY = st.secrets["FIREBASE_API_KEY"]
+
+except Exception:
+    FIREBASE_API_KEY = os.getenv("FIREBASE_API_KEY")
